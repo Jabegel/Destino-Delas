2eec31f8991d721e1eee72464592a1c68932eb41
João Gabriel <joaogabriel.dos.s.pereira@gmail.com>
2025-06-21 20:41:57 -0300

    js dnv

--- script.js
+++ script.js
@@ -3,3 +3,14 @@ function toggleMenu() {
     const menu = document.querySelector('.menu');
     menu.classList.toggle('active');
 }
+
+function toggleDetalhes(id) {
+    const detalhesDiv = document.getElementById(`detalhes-${id}`);
+    // Se você usa a lógica de fechar outros menus:
+    document.querySelectorAll('.detalhes-viagem.show').forEach(div => {
+        if (div.id !== `detalhes-${id}`) {
+            div.classList.remove('show');
+        }
+    });
+    detalhesDiv.classList.toggle('show');
+}
