02e2b54a5cf3875ee18099a6a1bb069a4e039171
João Gabriel <joaogabriel.dos.s.pereira@gmail.com>
2025-07-20 12:54:34 -0300

    Ultimas feat

--- script.js
+++ script.js
@@ -11,4 +11,20 @@ function toggleDetalhes(id) {
     detalhesDiv.classList.toggle('show');
 }
 
+// MENU LATERAL
+document.addEventListener('DOMContentLoaded', function () {
+  const trigger = document.getElementById('menu-trigger');
+  const menu = document.getElementById('side-menu');
 
+  if (trigger && menu) {
+    trigger.addEventListener('click', () => {
+      menu.classList.toggle('active');
+    });
+
+    menu.querySelectorAll('a').forEach(link => {
+      link.addEventListener('click', () => {
+        menu.classList.remove('active');
+      });
+    });
+  }
+});
